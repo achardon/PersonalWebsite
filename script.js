ed330be305958449d3357d5a80caf8ce69ebca17
Alessandra Chardon <achardon@ucla.edu>
2021-08-15 20:54:53 -0400

    Added links and About Me info

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
 const h2 = document.createElement("h2");
 h2.textContent = "Welcome to my website!";
 document.querySelector("body").appendChild(h2);
+
+
+
+//Clicking on buttons brings you to correct part of site
+
+//testing
+
+const button = document.getElementById('button');
+
+function clickAlert() {
+  alert('I was clicked!');
+}
+
+button.addEventListener('click', clickAlert());
+
+//Clicking on buttons turns them different color
+const input = document.getElementById('button');
+
+function clickButtonColorChange() {
+    input.color = red;
+}
+
+input.addEventListener('click', clickButtonColorChange);
